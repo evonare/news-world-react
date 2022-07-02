01332a1279dd0876f3afbc2aa4782839308258a4
Nadia Iqbal <nadiaiqbal2600@example.com>
2022-07-03 00:10:42 +0500

    Project Setup - Component Based Structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
-  c = "john";
   render() {
     return (
       <div>
-        heloo {this.c}
+        <NavBar />
+        <News/>
       </div>
-    )
+    );
   }
 }
---
+++ src/components/NewsItem.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div className='text-info'>
+        This is a newsitem component.
+      </div>
+    )
+  }
+}
+
+export default NewsItem
