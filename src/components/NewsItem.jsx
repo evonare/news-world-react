8042aaebce18515cc5876f30b7955e2ede71304c
Nadiakhanv2 <nadiaiqbal2600@gmail.com>
2022-07-03 23:14:20 +0500

    Usestate

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,21 +1,22 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+ 
     render() {
-      let  { title, description }=this.props;
+      let  { title, description, imageUrl, newsUrl }=this.props;
     return (
-      <div className="text-info">
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
           <img
-            src="https://talksport.com/wp-content/uploads/sites/5/2022/07/crop-1141691.jpg?strip=all&quality=100&w=1920&h=1080&crop=1"
+            src={imageUrl}
             className="card-img-top"
             alt="..."
           />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetail" className="btn btn-primary">
-              Go somewhere
+            <a href="/newsdetail/" className="btn btn-sm btn-primary">
+              Read More
             </a>
           </div>
         </div>
---
+++ src/components/NewsMonkey.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NewsMonkey() {
+  return (
+    <div>
+          <header>
+            <h1 className=''>NewsMonkey</h1>
+</header>
+
+    </div>
+  )
+}
+
+export default NewsMonkey
