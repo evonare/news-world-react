81559d221b86935a9903e42f85b9a00cd85a3168
Nadiakhanv2 <nadiaiqbal2600@gmail.com>
2022-07-05 18:46:56 +0500

    Add Source to NewsItem

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -3,10 +3,13 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
  
     render() {
-      let  { title, description, imageUrl, newsUrl, author ,date }=this.props;
+      let  { title, description, imageUrl, newsUrl, author ,date ,source}=this.props;
     return (
       <div className="my-3">
         <div className="card">
+          <span className="position-absolute top-0 start-100 translate-middle badge bg-danger rounded-pill" style={{left: '90%' ,zIndex: '1'}}>
+            {source}
+          </span>
           <img
             src={
               !imageUrl
@@ -17,9 +20,14 @@ export class NewsItem extends Component {
             alt="..."
           />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
+            <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}...</p>
-            <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
             <a
               href={newsUrl}
               target="_blank"
