eac1156005e937834cab352db6200c608be7d6f2
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-10 13:13:01 +0500

    done

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,110 +1,123 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
+import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-import PropTypes from 'prop-types'
-
-
 const News = (props) => {
-const[articles,setArticles]=useState([]);
-const[loading,setLoading]=useState(true);
-const[page,setPage]=useState(1);
-const[totalResults,settotalResults]=useState(0);
- 
- const capitalizeFirstLetter = (string) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+
+  const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
-  
 
   // --life cycle method
-  const updateNews=async()=> {
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+  const updateNews = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=d63b28e83ad94ebe9c01ffe11d51f18f&page=${page}&pageSize=${props.pageSize}`;
     setLoading(true);
-    props.setProgress(10);
+    // props.setProgress(10);
     let data = await fetch(url);
     props.setProgress(30);
     let jsonData = await data.json();
     props.setProgress(70);
     setArticles(jsonData.articles);
-    settotalResults(jsonData.totalResults);
+    setTotalResults(jsonData.totalResults);
     setLoading(false);
-    
     props.setProgress(100);
-  }
-  useEffect(() => { 
+  };
+  useEffect(() => {
+    document.title = `${capitalizeFirstLetter(props.category)} - NewsWorld`;
     updateNews();
-      document.title = `${capitalizeFirstLetter(props.category)} - NewsWorld`;
     // eslint-disable-next-line
-  },[]);
+  }, []);
 
   
-  
-  const  fetchMoreData = async () => {
-
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
-    // setState({ loading: true });
+  const fetchMoreData = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${
+      props.country
+    }&category=${props.category}&apiKey=d63b28e83ad94ebe9c01ffe11d51f18f&page=${
+      page + 1
+    }&pageSize=${props.pageSize}`;
     setPage(page + 1);
-
     let data = await fetch(url);
     let jsonData = await data.json();
     setArticles(articles.concat(jsonData.articles));
-    settotalResults(jsonData.totalResults);
-    
-  };
-    return (
-      <div className="container my-3">
-        <h1 className="text-center my-3 mt-5" >
-          NewsWorld - Top {capitalizeFirstLetter(props.category)}{" "}
-          Headlines
-        </h1>
-        {loading && <Spinner />}
-        <InfiniteScroll
-          dataLength={articles.length}
-          next={fetchMoreData}
-          hasMore={articles.length !== totalResults}
-          loader={<Spinner />}
-        >
-          <div className="">
-            <div className="row">
-              {/* {!loading && */}
-              {articles.map((element) => {
-                return (
-                  <div className="col-md-4" key={element.url}>
-                    <NewsItem
-                      title={element.title ? element.title.slice(0, 45) : ""}
-                      description={
-                        element.description
-                          ? element.description.slice(0, 88)
-                          : ""
-                      }
-                      imageUrl={element.urlToImage}
-                      newsUrl={element.url}
-                      author={element.author}
-                      date={element.publishedAt}
-                      source={element.source.name}
-                    />
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </InfiniteScroll>
-      </div>
-    );
-}
- News.defaultProps = {
-    country: "in",
-    pageSize: 8,
-    category: "general",
-  };
-  News.propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
+    setTotalResults(jsonData.totalResults);
   };
 
+
+  return (
+    <>
+      <h1
+        className="text-center my-3 mt-5"
+        style={{ margin: "35px 0px", marginTop: "90px" }}
+      >
+        NewsWorld - Top {capitalizeFirstLetter(props.category)} Headlines
+      </h1>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
+          <div className="row">
+            {articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title : ""}
+                    description={element.description ? element.description : ""}
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
+            {/* {articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 45) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 88)
+                        : ""
+                    }
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })} */}
+          </div>
+        </div>
+      </InfiniteScroll>
+    </>
+  );
+}
+
+News.defaultProps = {
+  country: "in",
+  pageSize: 8,
+  category: "general",
+};
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
 
 export default News;
-
-
